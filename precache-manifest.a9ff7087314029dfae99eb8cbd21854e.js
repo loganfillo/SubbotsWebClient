fc8c6afc0bea9415ccc889eb1615fcf009d47d6a
logan <logan.fillo@gmail.com>
2019-09-06 18:04:16 -0700

    Updates

---
+++ precache-manifest.a9ff7087314029dfae99eb8cbd21854e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98f30d3f3cafc573c863ab76fc80edd7",
+    "url": "/subbots-website/index.html"
+  },
+  {
+    "revision": "96b4ffcf1ba3f89c5c0e",
+    "url": "/subbots-website/static/css/main.ba069e69.chunk.css"
+  },
+  {
+    "revision": "da7c544cd58340aac368",
+    "url": "/subbots-website/static/js/2.cc77c4f0.chunk.js"
+  },
+  {
+    "revision": "96b4ffcf1ba3f89c5c0e",
+    "url": "/subbots-website/static/js/main.a7ca3542.chunk.js"
+  },
+  {
+    "revision": "e425449b645ac95e366d",
+    "url": "/subbots-website/static/js/runtime~main.d76c3714.js"
+  }
+]);
